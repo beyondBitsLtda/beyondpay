8b79b804a70483b8c07272ac7e02b059eff3ed55
unknown <bryansf94@gmail.com>
2025-01-02 05:52:22 -0300

    update

--- src/main.c
+++ src/main.c
@@ -1,16 +1,37 @@
-#include <stdio.h>
+/*main.c:
+Centraliza o fluxo do programa.
+Faz uso dos arquivos de cabeçalho (.h) do diretório include/ para chamar funções implementadas em src/.*/
 
-int main() {
-    printf("Hello World!");
-  return 0;
+/*incluindo os arquivos que serão lidos no main*/
+/*Uma observação é que possivvelmente terá que ser revisto e implementado outros códigos*/
+
+#include <stdio.h>
+#include "finance.h"
+#include "input.h"
+#include "output.h"
+/*biblioteca para pegar o tempo da transação*/
+#include <time.h>
+
+
+
+int main () {
+  time_t tempo_atual;
+  struct tm *data_hora;
+  int ano, mes, dia, hora, minuto, segundo;
+
+
+
 }
 
 
 
 
-/*main.c:
-Centraliza o fluxo do programa.
-Faz uso dos arquivos de cabeçalho (.h) do diretório include/ para chamar funções implementadas em src/.*/
+
+
+
+
+
+
 
 
 
