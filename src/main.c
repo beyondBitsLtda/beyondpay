ce286738a81d362a2e37698ac5f657b22533a3b5
unknown <bryansf94@gmail.com>
2025-01-02 06:25:00 -0300

    Começano a construir a main com base no repositório - https://github.com/vivek081202/FinTrack-Expense_and_Budget_Analyzer/blob/main/src/main.c

--- src/main.c
+++ src/main.c
@@ -3,21 +3,29 @@ Centraliza o fluxo do programa.
 Faz uso dos arquivos de cabeçalho (.h) do diretório include/ para chamar funções implementadas em src/.*/
 
 /*incluindo os arquivos que serão lidos no main*/
-/*Uma observação é que possivvelmente terá que ser revisto e implementado outros códigos*/
+/*Uma observação é que possivelmente terá que ser revisto e implementado outros códigos*/
+// Verificar repositório "https://github.com/vivek081202/FinTrack-Expense_and_Budget_Analyzer/blob/main/src/main.c"
 
 #include <stdio.h>
 #include "finance.h"
 #include "input.h"
 #include "output.h"
-/*biblioteca para pegar o tempo da transação*/
-#include <time.h>
+
 
 
 
 int main () {
-  time_t tempo_atual;
-  struct tm *data_hora;
-  int ano, mes, dia, hora, minuto, segundo;
+  int escolha; 
+  float buget;
+
+  // Definindo uma estrutura para representar uma data
+
+  typedef struct {
+    int dia;
+    int mes;
+    int ano;
+  } Data;
+  
 
 
 
